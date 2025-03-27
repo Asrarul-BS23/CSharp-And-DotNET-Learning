8f948a7d588b849b492616ebe7d0e7ff3e8f76ee
Asrarul Hoque Eusha <asrarul.hoque@brainstation-23.com>
2025-03-27 11:04:59 +0600

    Add OOP coding practices

---
+++ ClassObject/Program.cs
@@ -0,0 +1,68 @@
+﻿class Vehicle
+{
+    private string modelName;
+    private int vehicleId;
+    public string ModelName
+    {
+        get { return modelName; }
+        set { modelName = value; }
+    }
+    public int VehicleId
+    {
+        get { return vehicleId; }
+        set { vehicleId = value; }
+    } 
+    public void Honk()
+    {
+        Console.WriteLine("Peep Peeeep Ppepepeeeeeeeep.");
+    }
+}
+
+class Car: Vehicle
+{
+    private int speed;
+    public Car(int speed)
+    { 
+        this.speed = speed; 
+    }
+    public int Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+}
+
+class Truck: Vehicle
+{
+    private int capacity;
+    public int Capacity
+    {
+        get{ return capacity; }
+        set { capacity = value; }
+    }
+    public void Honk()
+    {
+        Console.WriteLine("Tuuut tuuuuut...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car= new Car(65);
+        car.ModelName = "Lamborgini";
+        car.VehicleId = 10001;
+        Console.WriteLine("========CAR========");
+        Console.WriteLine($"Vehicle ID: {car.VehicleId} \nModel Name: {car.ModelName} \nSpeed: {car.Speed}");
+        car.Honk();
+
+        Truck truck = new Truck();   
+        truck.Capacity = 5000;
+        truck.VehicleId = 10002;
+        truck.ModelName = "Heunday";
+        Console.WriteLine("========TRUCK========");
+        Console.WriteLine($"Vehicle ID: {truck.VehicleId} \nModel Name: {truck.ModelName} \nCapacity: {truck.Capacity}");
+        truck.Honk();
+    }
+}
---
+++ InterfaceAbstractClass/Program.cs
@@ -0,0 +1,86 @@
+﻿interface IAnimal
+{
+    public void Sounds();
+}
+class Pig: IAnimal
+{
+    public void Sounds()
+    {
+        Console.WriteLine("Pig sounds: Wee Weee....");
+    }
+}
+abstract class Pet 
+{
+    public abstract void Eat();
+}
+class Dog: Pet
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Dog eat meat.");
+    }
+}
+class Cat: IAnimal
+{
+    public void Sounds()
+    {
+        Console.WriteLine("Cat sounds: meu meu...");
+    }
+}
+
+interface IEngine
+{
+    public void StartEngine();
+}
+
+interface IRun
+{
+    public void Run();
+}
+interface IBrake
+{
+    public void ApplyBrakes();
+}
+
+class Car : IEngine, IBrake, IRun
+{
+    public void StartEngine()
+    {
+        Console.WriteLine("Engine started.");
+    }
+
+    public void ApplyBrakes()
+    {
+        Console.WriteLine("Brakes applied.");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Car Running.");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pig = new Pig();
+        Console.WriteLine("========PIG========");
+        pig.Sounds();
+
+        Console.WriteLine("========CAR========");
+        Car car = new Car();
+        car.StartEngine();
+        car.Run();
+        car.ApplyBrakes();
+
+        Console.WriteLine("========DOG========");
+        Dog dog = new Dog();
+        dog.Eat();
+
+        Console.WriteLine("========CAT========");
+        Cat cat= new Cat();
+        cat.Sounds();
+    }
+}
