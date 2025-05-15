25ab49aee86f2b406f2b89d4a67815ad2c100349
Asrarul Hoque Eusha <asrarul.hoque@brainstation-23.com>
2025-05-15 12:52:07 +0600

    Add multi-threading practice code

---
+++ MultiThreadingPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System;
+namespace MultiThreadingPractice;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Here is the printer.");
+
+        Console.ReadKey();
+
+        Thread t = Thread.CurrentThread;
+        //By Default, the Thread does not have any name
+        //if you want then you can provide the name explicitly
+        t.Name = "Main Thread";
+        Console.WriteLine("Current Executing Thread Name : " + t.Name);
+        Console.WriteLine("Current Executing Thread Name : " + Thread.CurrentThread.Name);
+
+        Console.Read();
+    }
+}
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Thread t = Thread.CurrentThread;
+//        //By Default, the Thread does not have any name
+//        //if you want then you can provide the name explicitly
+//        t.Name = "Main Thread";
+//        Console.WriteLine("Current Executing Thread Name : " + t.Name);
+//        Console.WriteLine("Current Executing Thread Name : " + Thread.CurrentThread.Name);
+
+//        Console.Read();
+//    }
+//}
