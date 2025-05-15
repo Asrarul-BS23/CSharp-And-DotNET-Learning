595d38ab70104cce9c6c43fa73f073b117635109
Asrarul Hoque Eusha <asrarul.hoque@brainstation-23.com>
2025-05-15 12:51:16 +0600

    Add practice code of Enhanced Params and Span

---
+++ EnhancedParams/Program.cs
@@ -0,0 +1,105 @@
+﻿/*
+<========== Learning Params ============>
+1. It enables passing variable number of parameters to a function
+*/
+namespace CSharp13NewFeatures;
+using System.Collections.Generic;
+
+public class SalesData
+{
+    public List<int> Sales { get; set; }
+}
+public class FeedbackData
+{
+    public List<int> Feedback { get; set; }
+}
+public class InventoryData
+{
+    public Dictionary<int, string> Inventory { get; set; }
+}
+
+public class ReportGenerator{
+    public void GenerateReport(params object[] datasets)
+    {
+        int totalSales = 0;
+        int totalRatings = 0;
+        float averageRatings = 0.0f;
+        int numberOfFeedback = 0;
+        int totalProductInStock = 0;
+
+        if(datasets.Length == 0 || datasets == null)
+        {
+            Console.WriteLine("Invalid Dataset!");
+            return;
+        }
+
+        foreach(var dataset in datasets)
+        {
+            if(dataset == null)
+            {
+                Console.WriteLine("Null Dataset Passed!"); 
+                continue;
+            }
+            if(dataset is SalesData salesData)
+            {
+                foreach(var sales in salesData.Sales)
+                {
+                    totalSales += sales;
+                }
+                Console.WriteLine("Processed Sales Data.");
+            }
+            else if(dataset is FeedbackData feedbackData)
+            {
+                foreach (var feedback in feedbackData.Feedback)
+                {
+                    totalRatings += feedback;
+                    numberOfFeedback++;
+                }
+                Console.WriteLine("Processed Feedback Data.");
+            }
+            else if(dataset is InventoryData inventoryData)
+            {
+                foreach(var inventory in inventoryData.Inventory)
+                {
+                    totalProductInStock += inventory.Key;
+                }
+                Console.WriteLine("Processed Inventory Data");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Data Type Passed!");
+            }
+        }
+        averageRatings = numberOfFeedback == 0? 0: (float)totalRatings/numberOfFeedback;
+
+        Console.WriteLine("<============ Generated Sales Report ============>");
+        Console.WriteLine($"Total Sales: {totalSales}");
+        Console.WriteLine($"Total InStock product: {totalProductInStock}");
+        Console.WriteLine($"Average Ratings: {averageRatings}");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\n=========================");
+        List<int> sales = new List<int> {10000,12000,11000,14000};
+        SalesData salesData = new SalesData{Sales = sales};
+
+        List<int> feedback = new List<int> {3,4,5,4,3};
+        FeedbackData feedbackData = new FeedbackData{Feedback = feedback};
+
+        Dictionary<int, string> inventory = new Dictionary<int,string> {
+            {10, "Orange"},
+            {20, "Apple"},
+            {30, "Onion"},
+            {40, "Mango"},
+            {50, "Jackfruit"},
+            };
+        InventoryData inventoryData= new InventoryData{Inventory = inventory};
+
+        ReportGenerator generator = new ReportGenerator();
+        generator.GenerateReport(salesData, feedbackData, inventoryData);
+        Console.WriteLine("=========================");
+    }
+}
---
+++ SpanUnderstanding/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+<=========== Learning Span ==========>
+1. Span is like pointer in C/C++
+2. It's memory efficient while slicing as it don't require memory allocation or copying contents
+*/
+
+int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+Span<int> span = new Span<int>(array);
+Span<int> slice = span.Slice(0,4);
+Console.Write("Original Array: ");
+foreach (var item in span)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+Console.Write("Sliced Array: ");
+foreach(var item in slice)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+
+span[1] = 20;
+Console.Write("Array after modification in span: ");
+foreach (var item in array)
+{
+    Console.Write(item + " ");
+}
